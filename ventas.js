5c4e916859663a9df0825a816241e2f5265c2c40
MateoM2008 <mateomunozlema000@gmail.com>
2026-07-07 20:15:11 -0500

    agragr validaciones

--- ventas.js
+++ ventas.js
@@ -9,10 +9,46 @@ function calcularComision(numeroVenta, precioProducto) {
     return comision
 }
 
+function validarNumero(input) {
+
+    let mensaje = document.getElementById("error_" + input.id);
+    let valor = input.value.trim();
+
+    mensaje.innerHTML = "";
+
+    // Vacío
+    if (valor === "") {
+        mensaje.innerHTML = "Este campo es obligatorio.";
+        return false;
+    }
+
+    // Solo números
+    if (!/^[0-9]+$/.test(valor)) {
+        mensaje.innerHTML = "Solo se permiten números.";
+        return false;
+    }
+
+    // Máximo 5 dígitos
+    if (valor.length > 5) {
+        mensaje.innerHTML = "Máximo 5 dígitos.";
+        return false;
+    }
+
+    return true;
+}
+
 function calcular() {
-    let sueldoBaseEntero =recuperarFloat("txtSueldoBase");
-    let presioEntero =recuperarFloat("txtPrecio")
-    let ventasEntero =recuperarFloat("txtVentas")
+
+    let nuemroVentasStr = recuperaTexto("txtVentas")
+
+    if (nuemroVentasStr.length > 5) {
+        alert("Maximo 5 caracteres")
+        return;
+    }
+
+    let sueldoBaseEntero = recuperarFloat("txtSueldoBase");
+    let presioEntero = recuperarFloat("txtPrecio")
+    let ventasEntero = recuperarFloat("txtVentas")
 
 
     let comision = calcularComision(ventasEntero, presioEntero);
@@ -20,11 +56,11 @@ function calcular() {
     let total = sueldoBaseEntero + comision;
 
     let cpmResultadoSalarioBase = document.getElementById("spSueldoBase")
-    cpmResultadoSalarioBase.textContent=sueldoBaseEntero
+    cpmResultadoSalarioBase.textContent = sueldoBaseEntero
 
     let cpmResultadoComison = document.getElementById("spComision")
-    cpmResultadoComison.textContent=comision
+    cpmResultadoComison.textContent = comision
 
     let cpmResulTotal = document.getElementById("spTotal")
-    cpmResulTotal.textContent=total
+    cpmResulTotal.textContent = total
 }
